3344d40f599e883f62d89d0045436df7b7412ad8
Evan Cesanek <evan.cesanek@gmail.com>
2024-04-10 11:00:33 -0400

    Modify getResult function to return entire leda2 object

--- ledapy/runner.py
+++ ledapy/runner.py
@@ -23,7 +23,9 @@ from . import analyse
 from . import deconvolution
 
 
-def getResult(raw_vector, result_type, sampling_rate, downsample=1, optimisation=0, pipeout=None):
+def getResult(
+    raw_vector, result_type, sampling_rate, downsample=1, optimisation=0, pipeout=None
+):
     """
     Run main analysis: extract phasic driver (returned) and set all leda2 values
 
@@ -39,17 +41,17 @@ def getResult(raw_vector, result_type, sampling_rate, downsample=1, optimisation
     leda2.current.do_optimize = optimisation
     import_data(raw_vector, sampling_rate, downsample)
     deconvolution.sdeco(optimisation)
-    if result_type.lower() == 'phasicdata':
+    if result_type.lower() == "phasicdata":
         result = leda2.analysis.phasicData
-    elif result_type.lower() == 'phasicdriver':
+    elif result_type.lower() == "phasicdriver":
         result = leda2.analysis.driver
     else:
-        raise ValueError('result_type not recognised (was ' + result_type + ')')
+        raise ValueError("result_type not recognised (was " + result_type + ")")
     if pipeout is not None:
         pipeout.send(result)
         pipeout.close()
     else:
-        return result
+        return leda2  # result # Modified by EC to return complete leda2 object
 
 
 def import_data(raw_data, srate, downsample=1):
@@ -58,19 +60,25 @@ def import_data(raw_data, srate, downsample=1):
     Adapted from main/import/import_data.m
     """
     if not isinstance(raw_data, np.ndarray):
-        raw_data = npa(raw_data, dtype='float64')
+        raw_data = npa(raw_data, dtype="float64")
     time_data = utils.genTimeVector(raw_data, srate)
-    conductance_data = npa(raw_data, dtype='float64')
+    conductance_data = npa(raw_data, dtype="float64")
     if downsample > 1:
-        (time_data, conductance_data) = utils.downsamp(time_data, conductance_data, downsample, 'mean')
+        (time_data, conductance_data) = utils.downsamp(
+            time_data, conductance_data, downsample, "mean"
+        )
     leda2.data.samplingrate = srate / downsample
     leda2.data.time_data = time_data
     leda2.data.conductance_data = conductance_data
-    leda2.data.conductance_error = np.sqrt(np.mean(pow(np.diff(conductance_data), 2)) / 2)
+    leda2.data.conductance_error = np.sqrt(
+        np.mean(pow(np.diff(conductance_data), 2)) / 2
+    )
     leda2.data.N = len(conductance_data)
     leda2.data.conductance_min = np.min(conductance_data)
     leda2.data.conductance_max = np.max(conductance_data)
-    (leda2.data.conductance_smoothData, leda2.data.conductance_smoothData_win) = utils.smooth_adapt(conductance_data, srate, .00001)
+    (leda2.data.conductance_smoothData, leda2.data.conductance_smoothData_win) = (
+        utils.smooth_adapt(conductance_data, srate, 0.00001)
+    )
     analyse.trough2peak_analysis()
 
 
@@ -87,8 +95,8 @@ def calculateMinMax(driver, sample_rate, iscr_win, minmax_win, pipeout):
     pipeout : sends min and max through this pipe
     pipes out a tuple (iscr_min, iscr_max)
     """
-    iscr_min = float('inf')
-    iscr_max = float('-inf')
+    iscr_min = float("inf")
+    iscr_max = float("-inf")
     if minmax_win == 0:
         startpoint = -len(driver)
     else:
